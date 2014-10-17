479b84df5d313224520729cd089ded6639531a27
daniel.bradley <daniel.bradley@fundapps.co>
2014-10-17 15:19:33 +0100

    Initialise project from scaffold
    https://github.com/fsprojects/ProjectScaffold

---
+++ src/TodoMvcBackend/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("TodoMvcBackend")>]
+[<assembly: AssemblyProductAttribute("TodoMvcBackend")>]
+[<assembly: AssemblyDescriptionAttribute("Implementation of the TODO MVC Backend using F#, F# Data, OWIN and WebAPI.")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
---
+++ tests/TodoMvcBackend.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module TodoMvcBackend.Tests
+
+open TodoMvcBackend
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
